112b4cc954cb73606ae7e0d8d18c41c17c9abbc0
Thoai Pham <dinhthoai241297@gmail.com>
2019-01-28 09:27:29 +0700

    migrate new format project

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux';
+import SectorReducer from './SectorReducer';
+import MajorReducer from './MajorReducer';
+import MarkReducer from './MarkReducer';
+import ProvinceReducer from './ProvinceReducer';
+import SchoolReducer from './SchoolReducer';
+import SubjectReducer from './SubjectReducer';
+import SubjectGroupReducer from './SubjectGroupReducer';
+import RoleReducer from './RoleReducer';
+import UserReducer from './UserReducer';
+import LoginReducer from './LoginReducer';
+import StatusReducer from './StatusReducer';
+import DifReducer from './DifReducer';
+import NewReducer from './NewReducer';
+import MediaReducer from './MediaReducer';
+
+const root = combineReducers({
+    SectorReducer,
+    SchoolReducer,
+    SubjectReducer,
+    SubjectGroupReducer,
+    MajorReducer,
+    MarkReducer,
+    ProvinceReducer,
+    RoleReducer,
+    UserReducer,
+    LoginReducer,
+    StatusReducer,
+    DifReducer,
+    NewReducer,
+    MediaReducer
+});
+
+export default root;
